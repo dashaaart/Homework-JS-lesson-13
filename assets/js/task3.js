9ad77fda07f320488b3a32378bb5e327670a4157
Даша Артюкевич <dartykevich@bk.ru>
2020-06-30 19:05:02 +0300

    added a solution to problem 3

--- assets/js/task3.js
+++ assets/js/task3.js
@@ -1,22 +1,28 @@
+// 3* Сделайте калькулятор используя ветвления, prompt'ы 
+// и так далее(то, что успели пройти)
+
 var a = +prompt('Введите первое число'); 
 var b = +prompt('Введите второе число');
 var c = prompt('сложить? вычесть? умножить? поделить?');
 
-switch(calculator) {
+switch(c) {
     case 'сложить':
         var d = a + b;
         alert(d);
         break;
 
     case 'вычесть': 
-        alert(a - b);
+        var e = a - b;
+        alert(e);
         break;
 
     case 'умножить': 
+        var f = a * b;
         alert(a * b);
         break;
 
     case 'поделить': 
+        var g = a / b;
         alert(a / b);
         break;
 
